af7ca8c1d8587b17d977f23a0090e969229b8a34
Sajad Akbar <sajadny@yahoo.com>
2020-01-18 19:01:17 -0500

    adding

--- src/parser/UnitTestingStudentProfile.java
+++ src/parser/UnitTestingStudentProfile.java
@@ -1,8 +1,21 @@
 package parser;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class UnitTestingStudentProfile {
     public static void main(String[] args) {
-        //Apply Unit Test into all the methods in this package.
 
+
+        //Apply Unit Test into all the methods in this package.
+        try {
+            Student student = new Student();
+            student.firstName.toString();
+            Assert.assertEquals("John", "Doe", "does not match");
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
+
+
 }
